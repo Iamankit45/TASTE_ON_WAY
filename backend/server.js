0e137e0a2af874619200308c681d2c83a18468e4
ANKIT <kumarankit0327@gmail.com>
2023-09-26 00:29:22 +0530

    added allowed origins

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-const path =require('path');
+const path = require('path');
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
@@ -11,9 +11,23 @@ const app = express();
 const globalErrHandler = require("./middlewares/globalErrHandler");
 const port = process.env.PORT || 8000;
 
-const Razorpay=require('razorpay');
+const Razorpay = require('razorpay');
 
 app.use(credentials);
+
+// Allow requests from specific origins (replace with your actual frontend URL)
+const allowedOrigins = ['https://taste-on-way.netlify.app'];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
+
 app.use(cors(corsOptions));
 // app.use(cors());
 app.use(cookieParser());
@@ -23,7 +37,7 @@ app.use(express.json()); //pass incoming payload
 
 
 exports.instance = new Razorpay({
-  
+
   key_id: process.env.RAZORPAY_API_KEY,
   key_secret: process.env.RAZORPAY_APT_SECRET,
 });
@@ -31,21 +45,21 @@ const userRouter = require("./routes/users/userRoutes");
 
 
 
-const foodRouter=require("./routes/foods/foodRoutes")
+const foodRouter = require("./routes/foods/foodRoutes")
 const paymentRouter = require("./routes/payments/payment");
 //routing of users,posts,category,comments
 app.use("/api/v1/users", userRouter);
 
 
-app.use("/api/v1/food",foodRouter);
+app.use("/api/v1/food", foodRouter);
 
-app.use("/api/v1/payment",paymentRouter);
+app.use("/api/v1/payment", paymentRouter);
 //errorhandlers middleware
 app.use(globalErrHandler);
 
 
-app.use("/api/v1/getkey",(req,res) => {
-  res.status(200).json({key:process.env.RAZORPAY_API_KEY});
+app.use("/api/v1/getkey", (req, res) => {
+  res.status(200).json({ key: process.env.RAZORPAY_API_KEY });
 })
 //404 error
 
